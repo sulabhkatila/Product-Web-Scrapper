73315805e23dd23744a38bf791501f790642f7f7
Sulabh Katila <sulabhkatila@gmail.com>
2024-05-19 10:14:44 -0400

    Initial commit for client

---
+++ client/index.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('https://www.freecodecamp.org/');
+	
+	await browser.close();
+})();
